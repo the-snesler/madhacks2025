3c4b461f0cd7aab0ca001648e4f15165cbeb5205
Adrian Brady <mail@adrianbrady.com>
2025-11-22 16:31:46 -0600

    update main

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,10 +1,17 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(|| async { "Server is up" }))
+        .route("/api", get(json));
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
 }
